90f8436bf40852c654d48c0d1dbe0c1042206855
Daniel Marin <danmarcab@gmail.com>
2014-03-22 20:22:23 +0000

    Added map objects to spatial hash. Closes #5

--- lib/map.rb
+++ lib/map.rb
@@ -1,17 +1,31 @@
 require 'yaml'
+require 'active_support/core_ext/hash/indifferent_access'
+require 'active_support/hash_with_indifferent_access'
 
 class Map
-  attr_accessor :width, :length, :objects
+  attr_accessor :width, :length, :cell_size, :objects
 
   def load(file)
-    map_info = YAML::load(File.open(File.expand_path(file)))
+    map_info = HashWithIndifferentAccess.new(YAML::load(File.open(File.expand_path(file))))
 
     map_info.each do |key, value|
       self.send("#{key}=", value)
     end
 
+    @spatial_hash = SpatialHash.new(cell_size)
+
     objects.each do |object_id, object|
+      object = object.each do |k, v|
+        object[k] = Vector.elements(v) if v.is_a?(Array)
+      end
       objects[object_id] = MapObject.create(object['type'], object)
+      @spatial_hash.insert objects[object_id]
     end
+
+    self
+  end
+
+  def search_objects box
+    @spatial_hash.query(box)
   end
 end
